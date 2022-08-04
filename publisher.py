12a7a163bbd5e8b0ca71001cebd00090a3fc3569
Ankit Mehra <ankitmehra1986@gmail.com>
2022-08-03 21:09:13 -0400

    changed to class introduced timer

--- publisher.py
+++ publisher.py
@@ -7,26 +7,42 @@ import random
 import math
 import time
 import json
-from util import generate_data_dict
+from util import Util
 
-BROKER = 'mqtt.eclipseprojects.io'
-CLIENT = mqtt.Client()
-PORT = 1883         # port 1883 - unencrypted TCP
-TOPIC = 'relativeHumidity'
 
-print(f'Connecting to broker: {BROKER}')
-CLIENT.connect(BROKER, PORT)  
-
-CLIENT.loop_start()
-while True:
-    hum_dict = generate_data_dict()     # generate data point
-    hum_json_str = json.dumps(hum_dict) # formated json string
+class Publisher:
     
-    CLIENT.publish(
-        topic = TOPIC,
-        payload = hum_json_str
-    )
+    BROKER = 'mqtt.eclipseprojects.io'  
+    CLIENT = mqtt.Client()
+    PORT = 1883         # port 1883 - unencrypted TCP
+    
+    
+    def __init__(self, delay:float = 0.5, topic:str = 'relativeHumidity') -> None:
+        self.gen = Util()
+        self.client = mqtt.Client()
+        self.topic = topic
+        self.delay = delay
+    
+    
+    def publish(self,times=20):
+        for x in range(times):
+            print(f'#{x}', end=' ' )
+            self.__publish()
+    
+     
+    def __publish(self):
+        Publisher.CLIENT.loop_start()
+        hum_dict = self.gen.generate_data_dict()     # generate data point
+        hum_json_str = json.dumps(hum_dict) # formated json string
+        print(f'Connecting to broker: {Publisher.BROKER}')
+        Publisher.CLIENT.connect(Publisher.BROKER, Publisher.PORT)  
+        Publisher.CLIENT.publish(
+            topic = self.topic,
+            payload = hum_json_str
+        )
+        print(f'Publishing to topic: {self.topic}\nWith payload: {hum_json_str} + to {Publisher.BROKER}')
+        time.sleep(5)
 
-    print(f'Publishing to topic: {TOPIC}\nWith payload: {hum_json_str} + to {BROKER}')
-    time.sleep(5)
 
+pub = Publisher()
+pub.publish(10)
