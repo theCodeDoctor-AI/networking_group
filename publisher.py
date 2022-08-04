835e57bd03e432aad241b5d0a42ebb07778be116
Ankit Mehra <ankitmehra1986@gmail.com>
2022-08-03 21:21:15 -0400

    proper spacing and indentation

--- publisher.py
+++ publisher.py
@@ -11,34 +11,30 @@ from util import Util
 
 
 class Publisher:
-    
-    BROKER = 'mqtt.eclipseprojects.io'  
+    BROKER = 'mqtt.eclipseprojects.io'
     CLIENT = mqtt.Client()
-    PORT = 1883         # port 1883 - unencrypted TCP
-    
-    
-    def __init__(self, delay:float = 0.5, topic:str = 'relativeHumidity') -> None:
+    PORT = 1883  # port 1883 - unencrypted TCP
+
+    def __init__(self, delay: float = 0.5, topic: str = 'relativeHumidity') -> None:
         self.gen = Util()
         self.client = mqtt.Client()
         self.topic = topic
         self.delay = delay
-    
-    
-    def publish(self,times=20):
+
+    def publish(self, times=20):
         for x in range(times):
-            print(f'#{x}', end=' ' )
+            print(f'#{x}', end=' ')
             self.__publish()
-    
-     
+
     def __publish(self):
         Publisher.CLIENT.loop_start()
-        hum_dict = self.gen.generate_data_dict()     # generate data point
-        hum_json_str = json.dumps(hum_dict) # formated json string
+        hum_dict = self.gen.generate_data_dict()  # generate data point
+        hum_json_str = json.dumps(hum_dict)  # formatted json string
         print(f'Connecting to broker: {Publisher.BROKER}')
-        Publisher.CLIENT.connect(Publisher.BROKER, Publisher.PORT)  
+        Publisher.CLIENT.connect(Publisher.BROKER, Publisher.PORT)
         Publisher.CLIENT.publish(
-            topic = self.topic,
-            payload = hum_json_str
+            topic=self.topic,
+            payload=hum_json_str
         )
         print(f'Publishing to topic: {self.topic}\nWith payload: {hum_json_str} + to {Publisher.BROKER}')
         time.sleep(5)
--- util.py
+++ util.py
@@ -1,29 +1,29 @@
-'''
+"""
     Utility script for MQTT application
     Houses Util functions for publisher
-    
-    Monthly Relative humidity for toronto varies roughly from a minimum of 14% 
-    to a maximun of 100%. Therefore a base value of 57% i.e a average of min and
+
+    Monthly Relative humidity for toronto varies roughly from a minimum of 14%
+    to a maximum of 100%. Therefore, a base value of 57% i.e a average of min and
     max humidity can be assumed.
     Source:- https://toronto.weatherstats.ca/charts/relative_humidity-monthly.html
-'''
+"""
 
 import random
 import math
 import time
 
+
 class Util:
-    
+
     def __init__(self) -> None:
         self.start_id = 11
         self.base = 57
-    
 
-    def generate_data_dict(self)->dict:
-        '''Returns a data dictionary containing a timestamp as well as a relative humidity value'''
+    def generate_data_dict(self) -> dict:
+        """Returns a data dictionary containing a timestamp as well as a relative humidity value"""
         self.start_id += 1
-        noise = random.randint(-42, 42)   # so that humidty remains in the range of 14 to 100%
-        
+        noise = random.randint(-42, 42)  # so that humidty remains in the range of 14 to 100%
+
         # base = random.randint(20, 90)           # generate random humiduty value between 10-90
         humidity = math.sin(self.base) + self.base + noise
 
@@ -31,10 +31,9 @@ class Util:
             'id': self.start_id,
             'timestamp': time.asctime(),
             'humidity': humidity
-        }    
+        }
 
         return humidity_dict
-
 
 
 if __name__ == '__main__':
@@ -42,5 +41,5 @@ if __name__ == '__main__':
     #     print(generate_data_dict())
 
     # print(int(time.asctime()[-7:-5]) / 7.5)
-    list = []
-    print(list.pop())
+    new_list = []
+    print(new_list.pop())
