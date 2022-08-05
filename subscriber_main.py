bf97f72fd4771991a9f720910b4b1fa6340e7b03
Ankit Mehra <ankitmehra1986@gmail.com>
2022-08-04 23:37:44 -0400

    designed subscriber .. later gui could be added

---
+++ subscriber_main.py
@@ -0,0 +1,65 @@
+from pyexpat.errors import messages
+import paho.mqtt.client as mqtt
+import json
+import time
+import sys
+import threading
+from tkinter import Tk, Canvas, Frame, BOTH, W, Button, Label
+
+# Subscriber.py
+
+# brokers
+onlineBroker = "mqtt.eclipseprojects.io"
+localBroker = "localhost"
+topic = "relative-Humidity"
+
+# create client
+client = mqtt.Client()
+
+DATA_COUNT = 40
+data_queue = []  # to store the received data
+
+
+# decode and print the message when received
+def on_message(client, userdata, message):
+    data = message.payload.decode("utf-8")
+    rec_obj = json.loads(data)
+    updated_time = rec_obj["timestamp"]
+    data_queue.append(rec_obj["humidity"])  # append new data to the queue
+    print(f'Message received:  {updated_time}\nHumidity Data:{data_queue[-1]}')
+    print("data_queue: ", data_queue)
+    if len(data_queue) >= DATA_COUNT:  # if data is full, pop the first one
+        data_queue.pop(0)
+
+
+def run_forever() -> None:
+    try:
+        print("Press CTRL+C to disconnect")
+        client.loop_forever()
+        # client.loop_start()
+    except:
+        print("Disconnecting from broker")
+    client.disconnect()
+
+
+def start_client() -> None:
+    client.on_message = on_message
+    # connect to broker
+    # print(client.connect (broker, 1883))
+    if client.connect(onlineBroker, 1883) == 0:
+
+        client.subscribe(topic)
+        print(f'Subscribing to {onlineBroker}')
+        time.sleep(2)
+        # run_forever()
+    else:
+        print("Could not connect to MQTT broker")
+        sys.exit(-1)
+
+    run_forever()
+    # client.loop_start()
+    # client.disconnect()
+
+
+if __name__ == "__main__":
+    start_client()
