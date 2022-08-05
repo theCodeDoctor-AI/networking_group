336a11811c8fb3bd198f6836ff1b2da866c6db0f
Ankit Mehra <ankitmehra1986@gmail.com>
2022-08-04 23:35:45 -0400

    made changes to documentation

--- util.py
+++ util.py
@@ -4,7 +4,7 @@
 
     Monthly Relative humidity for toronto varies roughly from a minimum of 14%
     to a maximum of 100%. Therefore, a base value of 57% i.e a average of min and
-    max humidity can be assumed.
+    max humidity and a fluctation range of (-43,43) can be assumed.
     Source:- https://toronto.weatherstats.ca/charts/relative_humidity-monthly.html
 """
 
@@ -22,7 +22,7 @@ class Util:
     def generate_data_dict(self) -> dict:
         """Returns a data dictionary containing a timestamp as well as a relative humidity value"""
         self.start_id += 1
-        noise = random.randint(-42, 42)  # so that humidty remains in the range of 14 to 100%
+        noise = random.randint(-42, 42)  # To keep humidty in the range of 14 to 100%
 
         # base = random.randint(20, 90)           # generate random humiduty value between 10-90
         humidity = math.sin(self.base) + self.base + noise
